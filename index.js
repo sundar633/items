2a1f2717d82c7b94bbd725de1bd839794cd2a3b5
Sundar reddy <164499162+sundar633@users.noreply.github.com>
2025-11-09 14:23:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import express from "express";
 import fs from "fs";
-import cors from "cors";           // <— add this
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());                  // <— allow all domains
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req,res)=>{
   res.send("API working");
 });
 
-app.get("/api/chocolates",(req,res)=>{
-  const data = JSON.parse(fs.readFileSync("./data/chocolates.json","utf8"));
+app.get("/api/all",(req,res)=>{
+  const data = JSON.parse(fs.readFileSync("./items.json","utf8"));
   res.json(data);
 });
 
