af079e91016662bea3f8c5d81fd9ef258c0ed884
Sundar reddy <164499162+sundar633@users.noreply.github.com>
2025-11-09 14:28:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ app.get("/", (req,res)=>{
 });
 
 app.get("/api/all",(req,res)=>{
-  const data = JSON.parse(fs.readFileSync("./items.json","utf8"));
-  res.json(data);
+  try {
+    const text = fs.readFileSync("./items.json","utf8");
+    const data = JSON.parse(text);
+    res.json(data);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({error:"cannot read items.json"});
+  }
 });
 
 app.listen(PORT, ()=> {
